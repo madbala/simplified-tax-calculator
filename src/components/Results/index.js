40533eb2531772da9c23d0c71a5c9665e66c8f7b
Balaguru Ravi <balaguru@litmus7.com>
2023-04-23 01:07:27 +0530

    graph component added and minor bugs fixed

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,11 +1,13 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "@/context/GlobalState";
-import {toIndianCurrency} from "@/utils" 
+import { toIndianCurrency } from "@/utils"
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 
 const Result = () => {
   const { transactions } = useContext(GlobalContext);
-  const { grossSalary, hra, actual80C, actual80Dself, actual80DParent } =
+  const { grossSalary, hra, actual80C, actual80Dself, actual80DParent, lta } =
     transactions;
 
   const standardDeductions = 50000;
@@ -13,6 +15,7 @@ const Result = () => {
   oldRegimeDeductions =
     parseInt(standardDeductions) +
     parseInt(hra) +
+    parseInt(lta) +
     parseInt(actual80C) +
     parseInt(actual80Dself) +
     parseInt(actual80DParent);
@@ -56,9 +59,44 @@ const Result = () => {
     //slab 4 is pending
     return incomeTax;
   };
-  return (
+
+  const suggestedTaxRegime = oldRegimeTaxCalcuation() > newRegimeTaxCalcuation() ? `Old Tax Regime` : oldRegimeTaxCalcuation() == newRegimeTaxCalcuation() ? `Both Tax Regime values are similar` : `New Tax Regime`
+  const data = [
+    {
+      name: 'Old Tax Regime',
+      ["Gross total income"]: grossSalary,
+      Deductions: oldRegimeDeductions,
+      ["Net taxable income"]: netTaxableIncome_OLD,
+      ["Total income tax"]: oldRegimeTaxCalcuation()
+    },
+    {
+      name: 'New Tax Regime',
+      ["Gross total income"]: grossSalary,
+      Deductions: standardDeductions,
+      ["Net taxable income"]: netTaxableIncome_NEW,
+      ["Total income tax"]: newRegimeTaxCalcuation()
+    }
+  ];
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+
+      return (
+        <div>
+          <h1 className="bold">{label}</h1>
+          <p >{`Gross Total Income : ${toIndianCurrency(grossSalary)}`}</p>
+          <p >{`Deductions : ${label.toLowerCase().includes('old')?toIndianCurrency(oldRegimeDeductions):toIndianCurrency(standardDeductions)}`}</p>
+          <p >{`Net Taxable Income : ${label.toLowerCase().includes('old')?toIndianCurrency(netTaxableIncome_OLD):toIndianCurrency(netTaxableIncome_NEW)}`}</p>
+          <p >{`Total Income Tax : ${label.toLowerCase().includes('old')?toIndianCurrency(oldRegimeTaxCalcuation()):toIndianCurrency(newRegimeTaxCalcuation())}`}</p>
+        </div>
+      );
+    }
+
+    return null;
+  };
+
+  return (<>
     <div className="w-3/4 h-full pl-10">
-      <div className="flex flex-col h-full">
+      <div className="flex flex-col h-3/4">
         <div className="w-full flex flex-row mb-2">
           <div className="w-3/5"></div>
           <div className="w-1/5 flex justify-center font-bold">
@@ -95,14 +133,14 @@ const Result = () => {
             {oldRegimeTaxCalcuation()}
           </div>
           <div className="w-1/5 flex justify-center rounded-none border bg-red-500">
-            10,000
+            {newRegimeTaxCalcuation()}
           </div>
         </div>
 
         <div className="border p-4 mt-2 mb-2">
           <p>
             Which tax regime is better?{" "}
-            <span className="text-green-700 font-bold">Old Tax Regime</span>
+            <span className="text-green-700 font-bold">{suggestedTaxRegime}</span>
           </p>
         </div>
 
@@ -126,8 +164,44 @@ const Result = () => {
           <div className="w-1/5 flex justify-center border">10,000</div>
         </div>
       </div>
+
+      <ResponsiveContainer width="100%" height="50%">
+        <BarChart
+        barSize={70}
+        barGap={30}
+        barCategoryGap={30}
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip content={<CustomTooltip />} />
+          <Legend />
+          <Bar dataKey="Gross total income" fill="#8884d8" />
+          <Bar dataKey="Deductions" fill="#82ca9d" />
+          <Bar dataKey="Net taxable income" fill="#ffc658" />
+          <Bar dataKey="Total income tax" fill="red" />
+        </BarChart>
+      </ResponsiveContainer>
+
+
     </div>
+
+
+
+  </>
   );
 };
 
 export default Result;
+
+
+
--- src/components/UserInputs/index.js
+++ src/components/UserInputs/index.js
@@ -6,9 +6,10 @@ import {GlobalContext} from "@/context/GlobalState";
 const UserInputs = () => {
   const [grossSalary, setGrossSalary] = useState(0);
   const [hra, SetHra] = useState(0);
+  const [lta,setLta] = useState(0)
   const [eightyC, setEightC] = useState(0);
-  const [eightyDSelf, setEightDSelf] = useState("");
-  const [eightyDParent, setEightDParent] = useState("");
+  const [eightyDSelf, setEightDSelf] = useState(0);
+  const [eightyDParent, setEightDParent] = useState(0);
 
   const {addTransaction}=useContext(GlobalContext)
 
@@ -27,6 +28,7 @@ const UserInputs = () => {
     const values={
       grossSalary,
       hra,
+      lta,
       actual80C,
       //parseInt
       actual80Dself:+actual80Dself,
@@ -37,7 +39,7 @@ const UserInputs = () => {
   }
 
   return (
-    <div className=" overflow-y-auto h-full w-1/4 h-full border-r border-gray-300 pr-10">
+    <div className=" overflow-y-auto h-full w-1/4 border-r border-gray-300 pr-10">
       <form onSubmit={onSubmit}>
 
         <div className="mt-5">
@@ -98,40 +100,48 @@ const UserInputs = () => {
           >
             LTA
           </label>
-          <input
+          <CurrencyInput
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="confirm-password"
             type="text"
+            value={lta}
+            prefix="₹"
+            decimalsLimit={2}
+            intlConfig={{ locale: 'en-IN', currency: 'INR' }}
+            onValueChange={(value, name) => setLta(value)}
             placeholder="LTA"
           />
         </div>
         <div className="mt-5" data-tooltip="This is a message about">
           <label
-            class="flex items-center space-x-2 block text-gray-700 font-bold mb-2"
+            className="flex items-center space-x-2 block text-gray-700 font-bold mb-2"
             htmlFor="phone"
           >
             <span>Section 80C</span>
 
-            <div class="relative group">
+            <div className="relative group">
               <svg
-                class="w-4 h-4 text-gray-500 cursor-pointer"
+                className="w-4 h-4 text-gray-500 cursor-pointer"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path d="M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 110-12 6 6 0 010 12zm0-6a1 1 0 00-1 1v3a1 1 0 002 0v-3a1 1 0 00-1-1z" />
               </svg>
-              <div class="absolute z-10 hidden w-32 p-2 mt-2 text-sm leading-tight text-gray-700 bg-white border rounded shadow-md group-hover:block">
+              <div className="absolute z-10 hidden w-32 p-2 mt-2 text-sm leading-tight text-gray-700 bg-white border rounded shadow-md group-hover:block">
                 This is some helpful information!
               </div>
             </div>
           </label>
-          <input
+          <CurrencyInput
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="phone1"
             type="text"
             value={eightyC}
-            onChange={(e) => setEightC(e.target.value)}
+            prefix="₹"
+            decimalsLimit={2}
+            intlConfig={{ locale: 'en-IN', currency: 'INR' }}
+            onValueChange={(value, name) => setEightC(value)}
             placeholder="Section 80C"
           />
         </div>
@@ -139,26 +149,32 @@ const UserInputs = () => {
           <label className="block text-gray-700 font-bold mb-2" htmlFor="phone">
             Section 80D (self and Family)
           </label>
-          <input
+          <CurrencyInput
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="phone2"
             type="text"
-            placeholder="max(Rs 25000)"
             value={eightyDSelf}
-            onChange={(e) => setEightDSelf(e.target.value)}
+            prefix="₹"
+            decimalsLimit={2}
+            intlConfig={{ locale: 'en-IN', currency: 'INR' }}
+            onValueChange={(value, name) => setEightDSelf(value)}
+            placeholder="max(Rs 25000)"
           />
         </div>
         <div className="mt-5">
           <label className="block text-gray-700 font-bold mb-2" htmlFor="phone">
             Section 80D (Senior Citizen)
           </label>
-          <input
+          <CurrencyInput
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="phone2"
+            id="phone3"
             type="text"
-            placeholder="max(Rs 50000)"
             value={eightyDParent}
-            onChange={(e) => setEightDParent(e.target.value)}
+            prefix="₹"
+            decimalsLimit={2}
+            intlConfig={{ locale: 'en-IN', currency: 'INR' }}
+            onValueChange={(value, name) => setEightDParent(value)}
+            placeholder="max(Rs 50000)"
           />
         </div>
 
